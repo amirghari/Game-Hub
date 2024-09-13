171c03e16643a715850e07c03a4eb47f491a655a
Amir Ghari <amir.ghari@metropolia.fi>
2024-09-13 19:21:46 +0300

    theme customized

--- src/them.ts
+++ src/them.ts
@@ -6,6 +6,20 @@ const config: ThemeConfig = {
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, colors: {gray: {
+  50: 'f9f9f9',
+  100: 'ededed',
+  200: 'd3d3d3',
+  300: 'b3b3b3',
+  400: 'a0a0a0',
+  500: '898989',
+  600: '666666',
+  700: '4d4d4d',
+  800: '333333',
+  900: '1a1a1a',
+  
+
+
+}} })
 
 export default theme
